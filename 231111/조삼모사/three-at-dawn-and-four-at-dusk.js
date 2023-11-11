45c6c91353c304709c69916e58382a4538513513
HyunLo Kim <leeas016@gmail.com>
2023-11-11 22:19:52 +0900

    Title: 조삼모사, Time: 736ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 231111/조삼모사/three-at-dawn-and-four-at-dusk.js
@@ -0,0 +1,45 @@
+const input = require('fs')
+.readFileSync('/dev/stdin')
+.toString()
+.split('\n');
+
+const N = Number(input.shift());
+const PList = input.map((e) => e.split(" ").map((e) => +e));
+const evening = Array(N);
+let answer = Infinity;
+
+const Calc = () => {
+  let morningSum = (eveningSum = 0);
+
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N; j++) {
+      if (evening[i] === false && evening[j] === false) {
+        morningSum += PList[i][j];
+      }
+      if (evening[i] === true && evening[j] === true) {
+        eveningSum += PList[i][j];
+      }
+    }
+  }
+
+  return Math.abs(morningSum - eveningSum);
+};
+
+const BFS = (index, count) => {
+  if (count === N / 2) {
+    answer = Math.min(answer, Calc());
+    return;
+  }
+
+  if (index === N) {
+    return;
+  }
+
+  BFS(index + 1, count);
+  evening[index] = true;
+  BFS(index + 1, count + 1);
+  evening[index] = false;
+};
+
+BFS(0, 0);
+console.log(answer);
