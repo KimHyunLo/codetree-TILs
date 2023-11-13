f01e10e532dc53138bbef0159e19d3d000c4bdde
HyunLo Kim <leeas016@gmail.com>
2023-11-13 23:19:46 +0900

    Title: 연결된 칸 찾기, Time: 304ms, Memory: 45MB, Status: Passed - CodeTree

--- 231113/연결된 칸 찾기/find-linked-cells.js
+++ 231113/연결된 칸 찾기/find-linked-cells.js
@@ -6,49 +6,44 @@ const input = require('fs')
 const N = +input.shift();
 const list = input.map((e) => e.split(" ").map((e) => +e));
 
-const visited = Array(N).fill(null).map(() => Array(N).fill(false));
+const visited = Array(N)
+  .fill(null)
+  .map(() => Array(N).fill(false));
 const connectList = [];
 
 const BFS = (i, j) => {
-    let count = 0;
-    const queue = [[i, j]];
-    visited[i][j] = true;
+  let count = 1;
+  const queue = [[i, j]];
+  visited[i][j] = true;
 
-    while (queue.length > 0) {
-        const [i, j] = queue.shift();
+  while (queue.length > 0) {
+    const [i, j] = queue.shift();
 
-        const directions = [
-            [i, j - 1],
-            [i, j + 1],
-            [i - 1, j],
-            [i + 1, j],
-        ];
+    const directions = [
+      [i, j - 1],
+      [i, j + 1],
+      [i - 1, j],
+      [i + 1, j],
+    ];
 
-        directions.forEach((dir) => {
-            const x = dir[0];
-            const y = dir[1];
+    directions.forEach(([x, y]) => {
+      if (x >= 0 && y >= 0 && x < N && y < N && visited[x][y] === false && list[x][y] === 1) {
+        queue.push([x, y]);
+        visited[x][y] = true;
+        count++;
+      }
+    });
+  }
 
-            if (x >= 0 && y >= 0 && x < N && y < N && visited[x][y] === false && list[x][y] === 1) {
-                queue.push([x, y]);
-                visited[x][y] = true;
-                count++;
-            }
-        })
+  return count;
+};
+
+for (let i = 0; i < N; i++) {
+  for (let j = 0; j < N; j++) {
+    if (visited[i][j] === false && list[i][j] === 1) {
+      connectList.push(BFS(i, j));
     }
-
-    return count;
-} 
-
-for (let i=0; i<N; i++) {
-    for (let j=0; j<N; j++) {
-        if (visited[i][j] === false) {
-            const result = BFS(i, j);
-
-            if (result !== 0) {
-                connectList.push(result);
-            }
-        }
-    }
+  }
 }
 
 connectList.sort((a, b) => a - b);
