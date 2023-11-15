5d21f7cb88fb13c1683fdb836ffde34bbdd2c9b9
HyunLo Kim <leeas016@gmail.com>
2023-11-15 18:01:21 +0900

    Title: 블럭쌓는 명령, Time: 237ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 231115/블럭쌓는 명령/block-stacking-commands.js
@@ -0,0 +1,17 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+
+const [N, K] = input.shift().split(" ").map((e) => +e);
+const list = input.map((e) => e.split(" ").map((e) => +e));
+const container = Array(N).fill(0);
+
+for (let i = 0; i < K; i++) {
+    const [A, B] = list[i];
+
+    for (let j = A - 1; j < B; j++) {
+        container[j]++;
+    }
+}
+
+container.sort((a, b) => a - b);
+const middle = container[Math.floor(K / 2)];
+console.log(middle);
