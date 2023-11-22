7d25877007c35e45325ef7f9c3a7e0014a92c058
HyunLo Kim <leeas016@gmail.com>
2023-11-22 17:01:02 +0900

    Title: 빙산의 일각, Time: 5011ms, Memory: 44MB, Status: Time Limit Exceed - CodeTree

--- 231122/빙산의 일각/the-tip-of-the-iceberg.js
+++ 231122/빙산의 일각/the-tip-of-the-iceberg.js
@@ -6,10 +6,11 @@ const input = require("fs")
 const N = +input.shift();
 const list = [...input].map((e) => +e);
 const max = Math.max(...list);
+const min = Math.min(...list);
 let answer = 0;
 
-for (let i= 1; i < max; i++) {
-    let current = list
+for (let i= max; i > min; i--) {
+    const current = list
     .map((e) => e - i > 0 ? e - i : "s")
     .join("")
     .replaceAll(RegExp(/s+/g), " ")
